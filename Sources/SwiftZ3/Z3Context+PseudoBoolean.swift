e43a6f6816ab42b771c7b76a22ae289d096a9661
Adrian Antkowski <thenext1p@gmail.com>
2025-08-07 11:54:01 +0200

    Align Pseudo-Boolean Documentation with project style

--- Sources/SwiftZ3/Z3Context+PseudoBoolean.swift
+++ Sources/SwiftZ3/Z3Context+PseudoBoolean.swift
@@ -3,99 +3,99 @@ import CZ3
 public extension Z3Context {
     // MARK: - Pseudo-Boolean
 
-    /**
-     Create constraint for at most k true variables.
-     args[0] + args[1] + ... + args[n] <= k
-    
-     - Parameters:
-       - arguments: An array of `Z3Bool` representing the boolean variables.
-       - k: The maximum number of true variables allowed.
-    
-     - SeeAlso: ``makeAtLeast(_:k:)``
-    */
+    /// Create a constraint asserting that at most `k` variables in `arguments` are true.
+    ///
+    /// This corresponds to the inequality:
+    /// `args[0] + args[1] + ... + args[n] <= k`
+    ///
+    /// - Parameters:
+    ///   - arguments: An array of `Z3Bool` representing the Boolean variables.
+    ///   - k: The maximum number of variables allowed to be true.
+    ///
+    /// - seealso: `makeAtLeast`
     func makeAtMost(_ arguments: [Z3Bool], k: Int32) -> Z3Bool {
         preparingArgsAst(arguments) { count, args in
             Z3Bool(context: self, ast: Z3_mk_atmost(context, count, args, UInt32(k)))
         }
     }
-    
-    /**
-     Create constraint for at least k true variables.
-     args[0] + args[1] + ... + args[n] >= k
-    
-     - Parameters:
-       - arguments: An array of `Z3Bool` representing the boolean variables.
-       - k: The minimum number of true variables required.
-    
-     - SeeAlso: ``makeAtMost(_:k:)``
-    */
+
+    /// Create a constraint asserting that at least `k` variables in `arguments` are true.
+    ///
+    /// This corresponds to the inequality:
+    /// `args[0] + args[1] + ... + args[n] >= k`
+    ///
+    /// - Parameters:
+    ///   - arguments: An array of `Z3Bool` representing the Boolean variables.
+    ///   - k: The minimum number of variables required to be true.
+    ///
+    /// - seealso: `makeAtMost`
     func makeAtLeast(_ arguments: [Z3Bool], k: Int32) -> Z3Bool {
         preparingArgsAst(arguments) { count, args in
             Z3Bool(context: self, ast: Z3_mk_atleast(context, count, args, UInt32(k)))
         }
     }
-    
-    /**
-     Create constraint for sum of coefficients being less than or equal to k.
-     coeffs[0]\*args[0] + coeffs[1]\*args[1] + ... + coeffs[n]\*args[n] <= k
-    
-     - Parameters:
-       - arguments: An array of `Z3Bool` representing the boolean variables.
-       - coefficients: An array representing the coefficients for each variable.
-       - k: The maximum value of the sum.
-    
-     - Precondition: The number of arguments must be equal to number of coefficients.
-    
-     - SeeAlso: ``makePbGe(_:coefficients:k:)``
-     - SeeAlso: ``makePbEq(_:coefficients:k:)``
-    */
+
+    /// Create a constraint asserting that a weighted sum of Boolean variables is less than or equal to `k`.
+    ///
+    /// This corresponds to the inequality:
+    /// `coeffs[0]*args[0] + coeffs[1]*args[1] + ... + coeffs[n]*args[n] <= k`
+    ///
+    /// - Parameters:
+    ///   - arguments: An array of `Z3Bool` representing the Boolean variables.
+    ///   - coefficients: An array of coefficients corresponding to each variable.
+    ///   - k: The maximum allowed value of the sum.
+    ///
+    /// - Precondition: The number of arguments must match the number of coefficients.
+    ///
+    /// - seealso: `makePbGe`
+    /// - seealso: `makePbEq`
     func makePbLe(_ arguments: [Z3Bool], coefficients: [Int32], k: Int32) -> Z3Bool {
         precondition(arguments.count == coefficients.count)
-        
+
         return preparingArgsAst(arguments) { count, args in
             Z3Bool(context: self, ast: Z3_mk_pble(context, count, args, coefficients, k))
         }
     }
-    
-    /**
-     Create constraint for sum of coefficients being greater than or equal to k.
-     coeffs[0]\*args[0] + coeffs[1]\*args[1] + ... + coeffs[n]\*args[n] >= k
-    
-     - Parameters:
-       - arguments: An array of `Z3Bool` representing the boolean variables.
-       - coefficients: An array representing the coefficients for each variable.
-       - k: The minimum value of the sum.
-    
-     - Precondition: The number of arguments must be equal to number of coefficients.
-    
-     - SeeAlso: ``makePbLe(_:coefficients:k:)``
-     - SeeAlso: ``makePbEq(_:coefficients:k:)``
-    */
+
+    /// Create a constraint asserting that a weighted sum of Boolean variables is greater than or equal to `k`.
+    ///
+    /// This corresponds to the inequality:
+    /// `coeffs[0]*args[0] + coeffs[1]*args[1] + ... + coeffs[n]*args[n] >= k`
+    ///
+    /// - Parameters:
+    ///   - arguments: An array of `Z3Bool` representing the Boolean variables.
+    ///   - coefficients: An array of coefficients corresponding to each variable.
+    ///   - k: The minimum required value of the sum.
+    ///
+    /// - Precondition: The number of arguments must match the number of coefficients.
+    ///
+    /// - seealso: `makePbLe`
+    /// - seealso: `makePbEq`
     func makePbGe(_ arguments: [Z3Bool], coefficients: [Int32], k: Int32) -> Z3Bool {
         precondition(arguments.count == coefficients.count)
-        
+
         return preparingArgsAst(arguments) { count, args in
             Z3Bool(context: self, ast: Z3_mk_pbge(context, count, args, coefficients, k))
         }
     }
-    
-    /**
-     Create constraint for sum of coefficients being equal to k.
-     coeffs[0]\*args[0] + coeffs[1]\*args[1] + ... + coeffs[n]\*args[n] = k
-    
-     - Parameters:
-       - arguments: An array of `Z3Bool` representing the boolean variables.
-       - coefficients: An array representing the coefficients for each variable.
-       - k: The exact value of the sum.
-    
-     - Precondition: The number of arguments must be equal to number of coefficients.
-    
-     - SeeAlso: ``makePbLe(_:coefficients:k:)``
-     - SeeAlso: ``makePbGe(_:coefficients:k:)``
-    */
+
+    /// Create a constraint asserting that a weighted sum of Boolean variables is equal to `k`.
+    ///
+    /// This corresponds to the equation:
+    /// `coeffs[0]*args[0] + coeffs[1]*args[1] + ... + coeffs[n]*args[n] = k`
+    ///
+    /// - Parameters:
+    ///   - arguments: An array of `Z3Bool` representing the Boolean variables.
+    ///   - coefficients: An array of coefficients corresponding to each variable.
+    ///   - k: The exact value the sum must match.
+    ///
+    /// - Precondition: The number of arguments must match the number of coefficients.
+    ///
+    /// - seealso: `makePbLe`
+    /// - seealso: `makePbGe`
     func makePbEq(_ arguments: [Z3Bool], coefficients: [Int32], k: Int32) -> Z3Bool {
         precondition(arguments.count == coefficients.count)
-        
+
         return preparingArgsAst(arguments) { count, args in
             Z3Bool(context: self, ast: Z3_mk_pbeq(context, count, args, coefficients, k))
         }
